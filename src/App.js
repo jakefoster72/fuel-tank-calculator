c8891fb431a6473a699ad5586dfe4f48c85f8e20
gusl67 <gus.lawrence@global.com>
2022-09-06 12:22:33 +0100

    commit test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-          hi="hi"
         >
           Learn React
         </a>
