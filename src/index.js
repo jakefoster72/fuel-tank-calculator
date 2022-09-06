d07ff1f9d74e8efc2b35f5db6e0214eb2fe2750c
ericacalogero <Erica.Calogero@global.com>
2022-09-06 12:27:06 +0100

    Made a very insightful comment

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //Hello, this is Erica.
   <React.StrictMode>
     <App />
   </React.StrictMode>
