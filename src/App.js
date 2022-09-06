04a0dbd4e316d1608bafc325b9b8aca464794133
gusl67 <gus.lawrence@global.com>
2022-09-06 14:10:37 +0100

    inputs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import InputWithLabel from './InputWithLabel';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function App() {
   return (
     <div>
-      <InputWithLabel id="location" label="Location:" value={userData.email} onInputChange={handleInput} ></InputWithLabel>
-      <InputWithLabel id="code-input" type="number" label="Code:" value={userData.code} onInputChange={handleInput} ></InputWithLabel>
-    </div>
-  );
-}
+      <InputWithLabel id="location" type="text" label="Location:" value= onInputChange={handleInput} ></InputWithLabel>
+      <InputWithLabel id="tank_size" type="number" label="Tank Size (L):" value= onInputChange={handleInput} ></InputWithLabel>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="fuel_type">
+          Fuel Type:
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Unleaded</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Diesal</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      </div>
+    );
+  }
 
 export default App;
